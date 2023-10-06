a4ab253116dfa2417f600d76416e0b9e715a4eac
williamh973 <126678879+williamh973@users.noreply.github.com>
2023-10-06 16:32:54 +0200

    Removed sensitive code

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,12 +1,6 @@
 export const environment = {
     production: false,
    firebaseConfig: {
-    apiKey: "AIzaSyAf3vw2WORCvdxZdmzXcUHElSnLu0v_dPI",
-    authDomain: "car-market-94484.firebaseapp.com",
-    projectId: "car-market-94484",
-    storageBucket: "car-market-94484.appspot.com",
-    messagingSenderId: "250417332360",
-    appId: "1:250417332360:web:7765c5d9ff3a708a95df3d",
-    measurementId: "G-3NCK3WHJ6B"
+
     }
 };
